86afbc86353f1653c056d580ca49e7b482dab84d
Eric Yarham <eric.yarham@gmail.com>
2020-12-12 13:18:58 -0500

    add my friend

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React, my friend
           </a>
         </header>
       </div>
